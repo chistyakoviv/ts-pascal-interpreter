219828a8380493bd33ae7ab5e983ed1005b68c6f
Ilya Chistyakov <ichistyakovv@gmail.com>
2021-04-04 12:41:57 +0500

    Refactored tests

--- __tests__/index.js
+++ __tests__/index.js
@@ -4,8 +4,9 @@ const Lexer = require('../dist/lexer.js').default;
 describe('interpreter', () => {
     it('checks the correctness of evaluating an expression', () => {
         // arrange
-        const expresseion = '14 + 2 * 3 - 6 / 2';
-        const interpreter = new Interpreter(new Lexer(expresseion));
+        const expression = '14 + 2 * 3 - 6 / 2';
+        const lexer = new Lexer(expression);
+        const interpreter = new Interpreter(lexer);
 
         // act
         const result = interpreter.expr();
