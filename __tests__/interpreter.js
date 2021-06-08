c678bd93a3262bda3f0298c1c92b667f196c1e05
Ilya Chistyakov <ichistyakovv@gmail.com>
2021-06-08 20:39:56 +0500

    Added checking expression evaluation

--- __tests__/interpreter.js
+++ __tests__/interpreter.js
@@ -12,6 +12,29 @@ describe('interpreter', () => {
         return interpreter;
     };
 
+    it('checks expressions evaluaton', () => {
+        [
+            ['3', 3],
+            ['2 + 7 * 4', 30],
+            ['7 - 8 / 4', 5],
+            ['14 + 2 * 3 - 6 / 2', 17],
+            ['7 + 3 * (10 / (12 / (3 + 1) - 1))', 22],
+            ['7 + 3 * (10 / (12 / (3 + 1) - 1)) / (2 + 3) - 5 - 3 + (8)', 10],
+            ['7 + (((3 + 2)))', 12],
+            ['- 3', -3],
+            ['+ 3', 3],
+            ['5 - - - + - 3', 8],
+            ['5 - - - + - (3 + 4) - +2', 10],
+        ].forEach((test) => {
+            const [expr, result] = test;
+            const interpreter = createInterpreter('BEGIN a := %expr% END.'.replace('%expr%', expr));
+
+            interpreter.interpret();
+            const globalScope = interpreter.getGlobalScope();
+            expect(globalScope.a).toBe(result);
+        });
+    });
+
     it(`evaluates symbol table to equal {'a': 2, 'x': 11, 'c': 27, 'b': 25, 'number': 2}`, () => {
         // arrange
         const src = `
